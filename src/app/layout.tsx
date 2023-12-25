97fdc0269d76e26603ed44f3b3ca2eaa9f9de846
Michał Bryk <galeriakoloru90210@gmail.com>
2023-12-25 23:41:10 +0100

    add theme toggler

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
+import { ThemeProvider } from '@/components/ThemeProvider'
 
 
 
@@ -17,9 +18,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <Header />
-        {children}</body>
+      <body className='bg-white dark:bg-[#1A1C29]'>
+        <ThemeProvider
+        attribute='class'
+        defaultTheme='system'
+        enableSystem
+        disableTransitionOnChange
+        >
+            <Header />
+        {children}
+        </ThemeProvider>
+        </body>
+      
     </html>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,20 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { ThemeToggler } from '@/components/ThemeToggler'
 
 
 function Header() {
   return (
     <header>
         <Link href="/">
-            <Image src="https://links.papareact.com/a943ae" alt="Disney Logo" width={120} height={100} className='cursor-pointer invert'/>
+            <Image src="https://links.papareact.com/a943ae" alt="Disney Logo" width={120} height={100} className='cursor-pointer invert-0 dark:invert'/>
         </Link>
+        <div className='flex space-x-2'>
+            {/* Genre Dropdown */}
+            {/* Search Input */}
+            <ThemeToggler />
+            {/* ThemeToggler */}
+        </div>
     </header>
   )
 }
