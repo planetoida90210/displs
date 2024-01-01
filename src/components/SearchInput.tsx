c5962957864de8e408ee056e38b134c5d00dbc03
Michał Bryk <galeriakoloru90210@gmail.com>
2024-01-01 10:47:11 +0100

    finish ui searchinput

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -4,6 +4,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation"
 import { useForm } from "react-hook-form";
 import * as z from "zod"
+import { Form, FormControl, FormField, FormItem} from "@/components/ui/form"
+import { Input } from "./ui/input";
 
 const formSchema = z.object({
     input: z.string().min(2).max(50),
@@ -23,9 +25,21 @@ function SearchInput() {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         console.log(values)
+        router.push(`/search/${values.input}`)
+        form.reset()
       }
   return (
-    <div>SearchInput</div>
+    <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField control={form.control} name='input' render={({field}) => (
+              <FormItem>
+                <FormControl>
+                    <Input placeholder='Search' {...field} />
+                </FormControl>
+              </FormItem>  
+            )} />
+        </form>
+    </Form>
   )
 }
 
